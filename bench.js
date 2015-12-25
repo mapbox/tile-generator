5bc6aefb2a06628ce00607028f46b7660d33a58d
Tim Channell <tim.channell@gmail.com>
2015-12-24 20:00:09 -0500

    add bench to compare with tile-cover for bboxes

---
+++ bench.js
@@ -0,0 +1,35 @@
+var Benchmark = require('benchmark');
+var TileGenerator = require('./');
+var tileCover = require('tile-cover');
+var bboxPolygon = require('turf-bbox-polygon');
+
+var bbox = [
+  -77.16110229492188,
+  38.785133984236815,
+  -76.89056396484375,
+  38.997841307500714
+];
+var bboxGeometry = bboxPolygon(bbox).geometry;
+
+var suite = new Benchmark.Suite('tile-generator');
+suite
+  .add('tile-generator#tile-cover control', function () {
+    var tiles = tileCover.tiles(bboxGeometry, {min_zoom: 18, max_zoom: 18});
+  })
+  .add('tile-generator#tile-generator', function (deferred) {
+    var tiles = [];
+    new TileGenerator(bbox, 18)
+      .on('data', function (dt) {
+        tiles.push(dt);
+      })
+      .on('end', function () {
+        deferred.resolve();
+      });
+  }, {defer: true})
+  .on('cycle', function (event) {
+    console.log(String(event.target));
+  })
+  .on('complete', function () {
+
+  })
+  .run();
